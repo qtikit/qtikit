55aed9294325bf33238e4d6e8e75499d404359de
Jimmy Moon <ragingwind@gmail.com>
2021-11-10 13:53:08 +0900

    Improve Prompt style

--- react/src/components/Prompt.tsx
+++ react/src/components/Prompt.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-import { PromptCharacteristics as PromptProps } from '@qtikit/model/src/qti2_2';
+import { PromptCharacteristics as PromptProps } from "@qtikit/model/src/qti2_2";
 
-const Prompt: React.FC<PromptProps | any> = props => {
+const Prompt: React.FC<PromptProps | any> = (props) => {
   return (
     <div>
-      <h4>Prompt</h4>
-      {props.children}
+      <h4>{props.children}</h4>
     </div>
   );
 };
