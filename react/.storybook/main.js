9a8357ab276bde4258d20fac92bc31e2dad7b130
Jimmy Moon <ragingwind@gmail.com>
2021-11-11 13:59:47 +0900

    Restructure interactions storybook

--- react/.storybook/main.js
+++ react/.storybook/main.js
@@ -1,15 +1,17 @@
 const path = require("path");
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   framework: "@storybook/react",
-  staticDirs: ["./public"],
+  staticDirs: ["../src/stories/public"],
   webpackFinal: async (config) => {
     config.resolve.plugins = [
       ...(config.resolve.plugins || []),
-      new TsconfigPathsPlugin({configFile: path.resolve(__dirname, '../tsconfig.json')}),
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, "../tsconfig.json"),
+      }),
     ];
     return config;
   },
