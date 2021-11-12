30a16a9c2e73be2b5a50f6d185306ea41c0c331a
Il9 <rntpwn90@gmail.com>
2021-11-12 16:50:03 +0900

    Implement Draggable

---
+++ packages/react/src/components/DragDrop/Draggable.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import {Current, useDragDropContext} from '.';
+
+const draggableStyle = {
+  cursor: 'move',
+  display: 'inline-block',
+};
+
+interface DraggableProps {
+  current: Current;
+}
+
+const Draggable: React.FC<DraggableProps> = ({current, children}) => {
+  const {setCurrent} = useDragDropContext();
+
+  const handleDragStart: React.DragEventHandler<HTMLDivElement> = () => {
+    setCurrent(current);
+  };
+
+  return (
+    <div draggable onDragStart={handleDragStart} style={draggableStyle}>
+      {children}
+    </div>
+  );
+};
+
+export default Draggable;
+export {DraggableProps};
