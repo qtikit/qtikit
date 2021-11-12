f62b56143404bb3a728f814eda385dfa49ec1bf1
Jimmy Moon <ragingwind@gmail.com>
2021-11-12 11:24:41 +0900

    Implement TextEntryInteraction #10

--- packages/react/src/interactions/TextEntryInteraction/TextEntryInteraction.tsx
+++ packages/react/src/interactions/TextEntryInteraction/TextEntryInteraction.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import {TextEntryInteractionCharacteristics as TextEntryInteractionProps} from '@qtikit/model/lib/qti2_2';
 
+import {getPlaceHolder} from '../../utils/interaction';
+
+const textStyle = {
+  fontSize: '1em',
+  border: 'solid 1px',
+  width: '6ex',
+};
+
 const TextEntryInteraction: React.FC<TextEntryInteractionProps | any> = props => {
   return (
-    <div>
-      <h3>Upload Interaction</h3>
+    <span>
+      <input type="text" style={textStyle} placeholder={getPlaceHolder(props)} />
       {props.children}
-    </div>
+    </span>
   );
 };
 
