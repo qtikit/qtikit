ea70e0ccc7e335830643b8f1d08a8fc58d6fd6eb
Il9 <rntpwn90@gmail.com>
2021-11-16 11:26:49 +0900

    Fix index export line break

--- packages/react/src/components/DragDrop/index.ts
+++ packages/react/src/components/DragDrop/index.ts
@@ -1,6 +1,4 @@
 export {default as Draggable} from './Draggable';
-
 export {default as Droppable} from './Droppable';
-
 export {default as DragDropContext} from './DragDropContext';
 export * from './DragDropContext';
