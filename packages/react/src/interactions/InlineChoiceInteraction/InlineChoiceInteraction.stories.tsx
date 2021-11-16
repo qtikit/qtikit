cbcdb9717ff73fe434cd63f0a9176cffc3a6a72b
Jimmy Moon <ragingwind@gmail.com>
2021-11-16 15:48:38 +0900

    Hand over response of inline choice to the host

--- packages/react/src/interactions/InlineChoiceInteraction/InlineChoiceInteraction.stories.tsx
+++ packages/react/src/interactions/InlineChoiceInteraction/InlineChoiceInteraction.stories.tsx
@@ -4,14 +4,20 @@ export default {
   title: 'InlineChoiceInteraction',
 };
 
+const onChange = (value: any) => {
+  console.log(JSON.stringify(value));
+};
+
 export const inline_choice = QtiViewerTemplate.bind({});
 
 inline_choice.args = {
   assessmentItemSrc: 'items/inline_choice.xml',
+  onChange,
 };
 
 export const inline_choice_math = QtiViewerTemplate.bind({});
 
 inline_choice_math.args = {
   assessmentItemSrc: 'items/inline_choice_math.xml',
+  onChange,
 };
--- packages/react/src/interactions/InlineChoiceInteraction/InlineChoiceInteraction.tsx
+++ packages/react/src/interactions/InlineChoiceInteraction/InlineChoiceInteraction.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {InlineChoiceInteractionCharacteristics as InlineChoiceInteractionProps} from '@qtikit/model/lib/qti2_2';
+import {UserInput} from '@qtikit/model/lib/user-input';
 
-const InlineChoiceInteraction: React.FC<InlineChoiceInteractionProps | any> = ({shuffle, required, ...props}) => {
+import {QtiViewerContext} from '../../QtiViewer';
+
+const InlineChoiceInteraction: React.FC<InlineChoiceInteractionProps | any> = ({
+  responseIdentifier,
+  shuffle,
+  required,
+  ...props
+}) => {
+  const {onChange} = useContext(QtiViewerContext);
   const [value, setValue] = React.useState('');
+
   const handleChange: React.ChangeEventHandler<HTMLSelectElement> = event => {
-    console.log('resonse', event.target.value);
-    setValue(event.target.value);
+    const choice = event.target.value;
+    const userInput: UserInput = {};
+
+    userInput[responseIdentifier] = [choice];
+    onChange(userInput);
+
+    setValue(choice);
   };
 
   return (
