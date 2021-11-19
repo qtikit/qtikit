dca07dca084753ad9df5fb7c32c74c46538433a6
Il9 <rntpwn90@gmail.com>
2021-11-19 17:26:35 +0900

    Implement OrderSimpleChoice

--- packages/react/src/components/SimpleChoice.tsx
+++ packages/react/src/components/SimpleChoice.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
 import {SimpleChoiceCharacteristics as SimpleChoiceProps} from '@qtikit/model/lib/qti2_2';
 
+import {createStyle} from '../utils/style';
 import {useInteractionStateContext} from '../interactions/InteractionState';
+import {Current, Draggable, Droppable, useDragDropContext} from './DragDrop';
+
+const OrderSimpleChoiceStyle = createStyle(({index, isDragging}: {index: number; isDragging: boolean}) => ({
+  order: index,
+  opacity: isDragging ? 0.01 : 1,
+}));
+
+const OrderSimpleChoice: React.FC<SimpleChoiceProps | any> = ({identifier, children}) => {
+  const {interactionState, setInteractionState} = useInteractionStateContext();
+  const {current} = useDragDropContext();
+
+  const handleDragEnter = (current: Current) => {
+    if (current.name === identifier) return;
+
+    setInteractionState({
+      ...interactionState,
+      [current.name]: interactionState[identifier],
+      [identifier]: interactionState[current.name],
+    });
+  };
+
+  return (
+    <div
+      style={OrderSimpleChoiceStyle({
+        index: Number(interactionState[identifier] ?? 0),
+        isDragging: current?.name === identifier,
+      })}>
+      <Draggable current={{name: identifier, value: identifier}}>
+        <Droppable onDragEnter={handleDragEnter}>{children}</Droppable>
+      </Draggable>
+    </div>
+  );
+};
 
 const SimpleChoice: React.FC<SimpleChoiceProps | any> = ({identifier, children}) => {
   const {interactionState, setInteractionState} = useInteractionStateContext();
