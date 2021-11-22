3458ae142448f11d277942396b87b7e7222ad68e
Jimmy Moon <ragingwind@gmail.com>
2021-11-22 15:31:05 +0900

    Implement media interaction

--- packages/react/src/interactions/MediaInteraction/MediaInteraction.tsx
+++ packages/react/src/interactions/MediaInteraction/MediaInteraction.tsx
@@ -2,12 +2,7 @@ import React from 'react';
 import {MediaInteractionCharacteristics as MediaInteractionProps} from '@qtikit/model/lib/qti2_2';
 
 const MediaInteraction: React.FC<MediaInteractionProps | any> = props => {
-  return (
-    <div>
-      <h3>Media Interaction</h3>
-      {props.children}
-    </div>
-  );
+  return <>{props.children}</>;
 };
 
 export default MediaInteraction;
