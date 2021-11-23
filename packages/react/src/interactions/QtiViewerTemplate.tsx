bb441be745d099efd8b41f8a7ef64f468ae28951
Il9 <rntpwn90@gmail.com>
2021-11-23 15:24:06 +0900

    Fix storybook base url vercel

--- packages/react/src/interactions/QtiViewerTemplate.tsx
+++ packages/react/src/interactions/QtiViewerTemplate.tsx
@@ -3,10 +3,12 @@ import {ComponentStory} from '@storybook/react';
 
 import QtiViewer from '../';
 
+const BASE_URL = process.env.VERCEL_URL ?? 'http://localhost:6006';
+
 export const QtiViewerTemplate: ComponentStory<typeof QtiViewer> = args => {
   const [inputState, setInputState] = React.useState({});
 
-  args.assessmentItemSrc = `http://localhost:6006/tests/${args.assessmentItemSrc}`;
+  args.assessmentItemSrc = `${BASE_URL}/tests/${args.assessmentItemSrc}`;
   args.inputState = inputState;
   args.onChange = (value: any) => {
     setInputState(value);
