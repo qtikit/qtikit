aca1b1eb40c36bd2d6b147d1dff0cad1024a59e7
Il9 <rntpwn90@gmail.com>
2021-11-23 15:43:48 +0900

    Fix base url

--- packages/react/src/interactions/QtiViewerTemplate.tsx
+++ packages/react/src/interactions/QtiViewerTemplate.tsx
@@ -3,14 +3,10 @@ import {ComponentStory} from '@storybook/react';
 
 import QtiViewer from '../';
 
-const BASE_URL = process.env.VERCEL_URL ?? 'http://localhost:6006';
-
-console.log(process.env);
-
 export const QtiViewerTemplate: ComponentStory<typeof QtiViewer> = args => {
   const [inputState, setInputState] = React.useState({});
 
-  args.assessmentItemSrc = `${BASE_URL}/tests/${args.assessmentItemSrc}`;
+  args.assessmentItemSrc = `/tests/${args.assessmentItemSrc}`;
   args.inputState = inputState;
   args.onChange = (value: any) => {
     setInputState(value);
