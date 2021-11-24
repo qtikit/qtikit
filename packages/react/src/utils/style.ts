34d8c70e57c2de84718c07228c6f7d0e8b2ec281
Il9 <rntpwn90@gmail.com>
2021-11-24 11:39:54 +0900

    Implement HotspotChoice

--- packages/react/src/utils/style.ts
+++ packages/react/src/utils/style.ts
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Coords} from '@qtikit/model/lib/qti2_2';
 
 type StyleCreator<Props> = (props: Props) => React.CSSProperties;
 
@@ -10,4 +11,27 @@ export function createStyle<Props>(style: React.CSSProperties | StyleCreator<Pro
   }
 
   return style;
+}
+
+export function createShapeStyle(coordsPattern: Coords['pattern']) {
+  const coords = coordsPattern.split(',').map(Number);
+
+  return {
+    circle: {
+      borderRadius: '50%',
+      left: `${coords[0] - coords[2]}px`,
+      top: `${coords[1] - coords[2]}px`,
+      width: `${coords[2] * 2}px`,
+      height: `${coords[2] * 2}px`,
+    },
+    rect: {
+      left: `${coords[0]}px`,
+      top: `${coords[1]}px`,
+      width: `${coords[2] - coords[0]}px`,
+      height: `${coords[3] - coords[1]}px`,
+    },
+    poly: {},
+    ellipse: {},
+    default: {},
+  };
 }
