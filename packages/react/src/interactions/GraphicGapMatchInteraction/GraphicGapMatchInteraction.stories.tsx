5ab7963c2f825e40b9e2a3125565cc8b3be8e2a6
Il9 <rntpwn90@gmail.com>
2021-11-25 19:55:05 +0900

    Implement GraphicGapMatchInteraction

---
+++ packages/react/src/interactions/GraphicGapMatchInteraction/GraphicGapMatchInteraction.stories.tsx
@@ -0,0 +1,17 @@
+import {QtiViewerTemplate} from '../QtiViewerTemplate';
+
+export default {
+  title: 'GraphicGapMatchInteractions',
+};
+
+export const graphic_gap_match = QtiViewerTemplate.bind({});
+
+graphic_gap_match.args = {
+  assessmentItemSrc: 'items/graphic_gap_match.xml',
+};
+
+export const graphic_gap_match_text = QtiViewerTemplate.bind({});
+
+graphic_gap_match_text.args = {
+  assessmentItemSrc: 'items/graphic_gap_match_text.xml',
+};
