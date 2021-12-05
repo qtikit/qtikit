5d83dbdf3f896964dc9fec64e4e194230eabf769
Jimmy Moon <ragingwind@gmail.com>
2021-12-06 08:08:31 +0900

    Remove unimplemented interactions

--- packages/react/src/interactions/index.ts
+++ packages/react/src/interactions/index.ts
@@ -2,40 +2,28 @@ import React from 'react';
 
 import {Props} from '../types/component';
 import {getPropsByElement} from '../utils/node';
-import AssociateInteraction from './AssociateInteraction';
 import ChoiceInteraction from './ChoiceInteraction';
-import CustomInteraction from './CustomInteraction';
-import DrawingInteraction from './DrawingInteraction';
 import ExtendedTextInteraction from './ExtendedTextInteraction';
 import GapMatchInteraction from './GapMatchInteraction';
-import GraphicAssociateInteraction from './GraphicAssociateInteraction';
 import GraphicGapMatchInteraction from './GraphicGapMatchInteraction';
-import GraphicOrderInteraction from './GraphicOrderInteraction';
 import HotspotInteraction from './HotspotInteraction';
 import HottextInteraction from './HottextInteraction';
 import MatchInteraction from './MatchInteraction';
 import MediaInteraction from './MediaInteraction';
 import OrderInteraction from './OrderInteraction';
-import SelectPointInteraction from './SelectPointInteraction';
 import SliderInteraction from './SliderInteraction';
 import TextEntryInteraction from './TextEntryInteraction';
 import InlineChoiceInteraction from './InlineChoiceInteraction';
 
 export const interactionElementNames = [
-  'associateInteraction',
   'choiceInteraction',
-  'customInteraction',
-  'drawingInteraction',
   'extendedTextInteraction',
   'gapMatchInteraction',
-  'graphicAssociateInteraction',
   'graphicGapMatchInteraction',
-  'graphicOrderInteraction',
   'hotspotInteraction',
   'hottextInteraction',
   'mediaInteraction',
   'orderInteraction',
-  'selectPointInteraction',
   'sliderInteraction',
   'textEntryInteraction',
   'inlineChoiceInteraction',
@@ -55,20 +43,14 @@ export function createInteractionComponent(
   const props = {...defaultProps, ...getPropsByElement(element)};
 
   const InteractionComponentMap: Record<InteractionElementName, React.FC> = {
-    associateInteraction: AssociateInteraction,
     choiceInteraction: ChoiceInteraction,
-    customInteraction: CustomInteraction,
-    drawingInteraction: DrawingInteraction,
     extendedTextInteraction: ExtendedTextInteraction,
     gapMatchInteraction: GapMatchInteraction,
-    graphicAssociateInteraction: GraphicAssociateInteraction,
     graphicGapMatchInteraction: GraphicGapMatchInteraction,
-    graphicOrderInteraction: GraphicOrderInteraction,
     hotspotInteraction: HotspotInteraction,
     hottextInteraction: HottextInteraction,
     mediaInteraction: MediaInteraction,
     orderInteraction: OrderInteraction,
-    selectPointInteraction: SelectPointInteraction,
     textEntryInteraction: TextEntryInteraction,
     inlineChoiceInteraction: InlineChoiceInteraction,
     sliderInteraction: SliderInteraction,
