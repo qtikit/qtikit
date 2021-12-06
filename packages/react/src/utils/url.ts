94d2608469b969db93aaf384ff342e5e6c86940e
Jimmy Moon <ragingwind@gmail.com>
2021-12-06 10:01:52 +0900

    Support external stylesheet

--- packages/react/src/utils/url.ts
+++ packages/react/src/utils/url.ts
@@ -1,3 +1,13 @@
 export function getBaseUrl(url: string): string {
-  return url.split('/').slice(0, -1).join('/');
+  if (!/^(http|https):\/\//.test(url)) {
+    throw new Error('Assessment source URL must be absolute path');
+  }
+
+  return url.split('/').slice(0, -1).join('/') + '/';
+}
+
+export function getUrl(pathname: string | null, baseurl?: string): string {
+  const url = new URL(baseurl || location.href);
+
+  return new URL(pathname || '/', url.origin).href;
 }
