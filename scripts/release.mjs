ce5df55703d3c8929c0b3af1c24ec4c102d4bf7c
Jimmy Moon <ragingwind@gmail.com>
2022-06-09 06:55:39 +0900

    chore: add target option to release script

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -3,6 +3,7 @@ import {spawnSync as spawn} from 'child_process';
 const ROOT = './.release/@qtikit';
 const PACKAGES = ['mathjax-react', 'model', 'react', 'scoring-engine'];
 const zipName = `@qtikit-${process.argv[2] ? `${process.argv[2]}` : ''}.zip`;
+const dest = process.argv[3];
 
 spawn('rm -rf ./.release', [], {shell: true, stdio: 'inherit'});
 
@@ -16,3 +17,8 @@ for (const packageName of PACKAGES) {
 spawn(`cd .release/ && zip -r -q ${zipName} * && cd - > /dev/null`, [], {shell: true, stdio: 'inherit'});
 
 console.log(`${zipName} created`);
+
+if (dest) {
+  spawn(`cp ./.release/${zipName} ${dest}`, [], {shell: true, stdio: 'inherit'});
+  console.log(`${zipName} copied to ${dest}`);
+}
